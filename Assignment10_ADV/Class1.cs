d8086c5026ccdbd9c3890498e6a3e7caaa3b21f0
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-07-30 05:33:35 +0300

    Add project files.

---
+++ Assignment10_ADV/Class1.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace Assignment10_ADV
+{
+    public class Reverse
+    {
+         public static void ReverseQueue(Queue<int> q)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            
+            while (q.Count > 0)
+            {
+                stack.Push(q.Dequeue());
+            }
+
+     
+            while (stack.Count > 0)
+            {
+                q.Enqueue(stack.Pop());
+            }
+        }
+    }
+}
---
+++ Assignment10_ADV/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace Assignment10_ADV
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            #region q1: Given an array  consists of  numbers with size N and number of queries, in each query you will be given an integer X, and you should print how many numbers in array that is greater than  X.
+
+            //string[] firstLine = Console.ReadLine()!.Split();
+            //    int N = int.Parse(firstLine[0]);
+            //    int Q = int.Parse(firstLine[1]);
+
+            //    int[] arr = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+
+            //    for (int i = 0; i < Q; i++)
+            //    {
+            //        int X = int.Parse(Console.ReadLine()!);
+            //        int count = 0;
+
+
+            //        foreach (int num in arr)
+            //        {
+            //            if (num > X)
+            //                count++;
+            //        }
+
+            //        Console.WriteLine(count);
+            //    }
+            #endregion
+
+
+            #region q2: Given a number N and an array of N numbers. Determine if it's palindrome or not.
+
+
+            //int N = int.Parse(Console.ReadLine()!);
+
+
+            //int[] arr = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+            //bool isPalindrome = true;
+
+            //for (int i = 0; i < N / 2; i++)
+            //{
+            //    if (arr[i] != arr[N - 1 - i])
+            //    {
+            //        isPalindrome = false;
+            //        break;
+            //    }
+            //}
+
+
+            //if (isPalindrome)
+            //    Console.WriteLine("YES");
+            //else
+            //    Console.WriteLine("NO");
+
+            #endregion
+
+
+            #region q3: Given a Queue, implement a function to reverse the elements of a queue using a stack.
+
+            //Queue<int> queue = new Queue<int>();
+
+            
+            //queue.Enqueue(5);
+            //queue.Enqueue(6);
+            //queue.Enqueue(4);
+            //queue.Enqueue(3);
+            //queue.Enqueue(2);
+
+            //Console.WriteLine("Original Queue: " + string.Join(" ", queue));
+
+            //Reverse.ReverseQueue(queue);
+
+            //Console.WriteLine("Reversed Queue: " + string.Join(" ", queue));
+
+            #endregion
+
+        }
+    }
+    
+}
+
+
